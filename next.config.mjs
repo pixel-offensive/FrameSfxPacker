f5230067d0c69bf3f1d4af8fc5a417e7e08462bd
yunfeidog <1844025705@qq.com>
2025-04-05 20:36:50 +0800

    ci: 更新 basePath 配置- 将 basePath 从 "/frame-sfx-packer" 修改为 "/FrameSfxPacker" -此修改统一了大小写，与其他配置保持一致

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/frame-sfx-packer",
+    basePath: "/FrameSfxPacker",
 
     /**
      * Disable server-based image optimization. Next.js does not support
